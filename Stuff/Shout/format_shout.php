1b47c28efd0b8a87dc159e57c4a43a53f1f83ff6
Karl <wukons@hotmail.com>
2013-09-14 10:04:46 +0300

    Create format_shout.php

---
+++ Stuff/Shout/format_shout.php
@@ -0,0 +1,73 @@
+<?php
+
+function format_shout($text, $strip_html = true) {
+
+    $s = $text;
+    //$s = strip_tags($s);
+
+  if ($strip_html)
+    $s = htmlspecialchars($s);
+
+    $s = stripslashes($s);
+
+    // [b]Bold[/b]
+    $s = preg_replace("/\[b\]((\s|.)+?)\[\/b\]/", "<b>\\1</b>", $s);
+
+    // [i]Italic[/i]
+    $s = preg_replace("/\[i\]((\s|.)+?)\[\/i\]/", "<i>\\1</i>", $s);
+
+    // [u]Underline[/u]
+    $s = preg_replace("/\[u\]((\s|.)+?)\[\/u\]/", "<u>\\1</u>", $s);
+
+    // [color=blue]Text[/color]
+    $s = preg_replace(
+        "/\[color=([a-zA-Z]+)\]((\s|.)+?)\[\/color\]/i",
+        "<font color=\\1>\\2</font>", $s);
+
+    // [color=#ffcc99]Text[/color]
+    $s = preg_replace(
+        "/\[color=(#[a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9])\]((\s|.)+?)\[\/color\]/i",
+        "<font color=\\1>\\2</font>", $s);
+
+    // [url=http://www.example.com]Text[/url]
+    $s = preg_replace(
+        "/\[url=((http|ftp|https|ftps|irc):\/\/[^<>\s]+?)\]((\s|.)+?)\[\/url\]/i",
+        "<a href=\\1 target=_blank>\\3</a>", $s);
+
+    // [url]http://www.example.com[/url]
+    $s = preg_replace(
+        "/\[url\]((http|ftp|https|ftps|irc):\/\/[^<>\s]+?)\[\/url\]/i",
+        "<a href=\\1 target=_blank>\\1</a>", $s);
+
+
+    // [url]www.example.com[/url]
+    $s = preg_replace(
+        "/\[url\](www\.[^<>\s]+?)\[\/url\]/i",
+        "<a href='http://\\1' target='_blank'>\\1</a>", $s);
+
+    // [url=www.example.com]Text[/url]
+    $s = preg_replace(
+        "/\[url=(www\.[^<>\s]+?)\]((\s|.)+?)\[\/url\]/i",
+        "<a href='http://\\1' target='_blank'>\\2</a>", $s);
+
+
+    // [size=4]Text[/size]
+    $s = preg_replace(
+        "/\[size=([1-7])\]((\s|.)+?)\[\/size\]/i",
+        "<font size=\\1>\\2</font>", $s);
+
+    // [font=Arial]Text[/font]
+    $s = preg_replace(
+        "/\[font=([a-zA-Z ,]+)\]((\s|.)+?)\[\/font\]/i",
+        "<font face=\"\\1\">\\2</font>", $s);
+
+    // Linebreaks
+    $s = nl2br($s);
+
+    // Maintain spacing
+    $s = str_replace("  ", " &nbsp;", $s);
+
+    return $s;
+}
+
+?>
